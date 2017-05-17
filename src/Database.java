8ae42bb38a61e1f3e29f4e68c8e0d4c7e67e4e9d
CaptainGeo <earthvexingtimelord@gmail.com>
2017-05-17 09:32:52 -0700

    Achievement Get: Can Write To File!

--- src/Database.java
+++ src/Database.java
@@ -6,11 +6,24 @@ import java.io.*;
  *
  */
 public class Database {
+
     public void writeItem(Item toWrite){
         String name = toWrite.getDisplayName();
         String desc = toWrite.getDescription();
         Item.Status status = toWrite.getStatus();
 
+        String write = name+"|"+status+"|"+desc+"\n";
+
+        //System.out.println(write);
+        byte data[] = write.getBytes(); //String -> Bytes
+        Path p = Paths.get("./database.txt");
+
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(p, CREATE, APPEND))) {
+            out.write(data, 0, data.length);
+        }
+        catch (IOException x) {
+            System.err.println(x);
+        }
     }
 
     public void test(){
@@ -26,7 +39,8 @@ public class Database {
         }
     }
     public static void main(String[] args) {
+        Item z= new Item("TestItem","This is a test");
         Database a= new Database();
-        a.test();
+        a.writeItem(z);
     }
 }
