3fcc1e0ab0bea964b55add20359c71e4031a0958
CaptainGeo <earthvexingtimelord@gmail.com>
2017-05-17 08:44:02 -0700

    created core classes and input interface

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Description
+ *
+ * @author Adrian Hardt & Logan Traffas
+ * @version 1.0.2
+ * @date 5/17/2017
+ * @assignment:
+ */
+public class Main {
+}
---
+++ src/UI.java
@@ -0,0 +1,10 @@
+/**
+ * Description
+ *
+ * @author Adrian Hardt
+ * @version 1.0.2
+ * @date 5/17/2017
+ * @assignment:
+ */
+public class UI {
+}
