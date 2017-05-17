461362228429cb26580c326ffb1917e67a53c595
CaptainGeo <earthvexingtimelord@gmail.com>
2017-05-17 09:16:54 -0700

    started work on database

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,31 @@
+import static java.nio.file.StandardOpenOption.*;
+import java.nio.file.*;
+import java.io.*;
+
 /**
  *
  */
 public class Database {
+    public void writeItem(Item toWrite){
+        String name = toWrite.getDisplayName();
+        String desc = toWrite.getDescription();
+
+    }
+
+    public void test(){
+        String s = "Hello World! ";
+        byte data[] = s.getBytes(); //String -> Bytes
+        Path p = Paths.get("./database.txt");
+
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(p, CREATE, APPEND))) {
+            out.write(data, 0, data.length);
+        }
+        catch (IOException x) {
+            System.err.println(x);
+        }
+    }
+    public static void main(String[] args) {
+        Database a= new Database();
+        a.test();
+    }
 }
