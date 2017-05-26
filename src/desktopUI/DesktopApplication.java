00a6e228f866756012dc45dbb55be6f57eb103e7
copernicium <logan.traffas@gmail.com>
2017-05-26 11:27:30 -0700

    Merge some files and change how content is updated so only the necessary nodes change, not the entire application

--- src/desktopUI/DesktopApplication.java
+++ src/desktopUI/DesktopApplication.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -21,8 +22,10 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import util.Pair;
-import util.Util;
 import util.Maybe;
+import util.Util;
+
+import java.awt.Dimension;
 
 /**
  * Runs the desktop application that displays the Items and provides for their management
@@ -30,31 +33,85 @@ import util.Maybe;
  * @author Logan Traffas
  */
 public class DesktopApplication extends Application{
-	private Stage mainStage;
+	//padding constants
+	private static final int EXPERIMENTAL_HORIZONTAL_INSETS = 0;//from testing with Windows 10
+	private static final int EXPERIMENTAL_VERTICAL_INSETS = 31;//from testing with Windows 10
+	private static final Dimension DEFAULT_SIZE = new Dimension(
+			1366 - EXPERIMENTAL_HORIZONTAL_INSETS,
+			768 - EXPERIMENTAL_VERTICAL_INSETS - util.Graphics.TASKBAR_HEIGHT
+	);
+	private static final int PADDING = 5;//px ?
+
+	//style/usability constants
 	private static final String STYLESHEET_SOURCE = "/res/DesktopApplicationStylesheet.css";
 	private static final int SECTION_HEIGHT = 75;
 	private static final boolean RESIZABLE = false;
+
 	public enum RightDisplay{ITEM_INFO,ADD_NEW_ITEM}
-	private Maybe<Item> activeItem;
 	private RightDisplay rightDisplay;
+
+	private Stage mainStage;
+
+	private Maybe<Item> activeItem;
 	private Database database;
+
 	private HBox rootPane;
 	private static final String ROOT_PANE_ID = "rootPane";
+
 	private VBox listPane;
 	private static final String LIST_PANE_ID = "listPane";
+
 	private VBox infoPane;
 	private static final String INFO_PANE_ID = "infoPane";
+
 	private VBox addItemPane;
 	private static final String ADD_ITEM_PANE_ID = "addItemPane";
+
+	private void replaceNode(String removeID,Node newNode){
+		//TODO
+	}
+
+
+	private void removeNode(String removeID){
+		removeID = "#" + removeID;
+		Node toRemove = this.rootPane.lookup(removeID);
+		if(toRemove != null) {
+			this.rootPane.getChildren().remove(toRemove);
+		}
+	}
+
+	/**
+	 * Builds the right side of the display, handling the fact that multiple different displays may appear there
+	 */
+	private void updateRightPane(){
+		removeNode(INFO_PANE_ID);//TODO: replace with replaceNode
+		removeNode(ADD_ITEM_PANE_ID);
+		switch(this.rightDisplay) {
+			case ITEM_INFO:
+				this.rootPane.getChildren().add(this.infoPane);
+				break;
+			case ADD_NEW_ITEM:
+				this.rootPane.getChildren().add(this.addItemPane);
+				break;
+			default:
+				Util.nyi(util.Util.getFileName(),util.Util.getLineNumber());
+		}
+	}
 
 	/**
 	 * Constructs the infoPane which is the detailed display for a selected Item
 	 */
-	private void constructInfoPane(){
+	private void updateInfoPane(){
 		this.infoPane.getChildren().clear();
-		this.addItemPane.getChildren().clear();
+		{
+			Node toRemove = this.rootPane.lookup(ADD_ITEM_PANE_ID);
+			if(toRemove != null) {
+				this.rootPane.getChildren().remove(toRemove);
+			}
+		}
+		//this.addItemPane.getChildren().clear();
 		final double WIDTH_PERCENT = .66;
-		final double WIDTH = StageConstants.DEFAULT_SIZE.width * WIDTH_PERCENT;
+		final double WIDTH = DEFAULT_SIZE.width * WIDTH_PERCENT;
 		this.infoPane.setMaxWidth(WIDTH);
 		this.infoPane.setPrefWidth(this.infoPane.getMaxWidth());
 		this.infoPane.getStyleClass().add("infoPane");
@@ -64,15 +121,15 @@ public class DesktopApplication extends Application{
 				itemDisplay.getStyleClass().add("itemDisplay");
 
 				final int BACKGROUND_HEIGHT = 605;//from testing on 5/25/17
-				final int BACKGROUND_WIDTH = (int) (WIDTH - (2 * StageConstants.PADDING));
+				final int BACKGROUND_WIDTH = (int) (WIDTH - (2 * PADDING));
 				Rectangle background = new Rectangle(BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
 				background.getStyleClass().add("itemDisplayBackground");
 
-				VBox itemDisplayInfoBorder = new VBox(StageConstants.PADDING);//used to align text in the upper left-hand corner with some padding
+				VBox itemDisplayInfoBorder = new VBox(PADDING);//used to align text in the upper left-hand corner with some padding
 				{
 					itemDisplayInfoBorder.getStyleClass().add("itemDisplayInfoBorder");
 					if(this.activeItem.isValid()){
-						final double WRAPPING_WIDTH = WIDTH - 4 * StageConstants.PADDING;
+						final double WRAPPING_WIDTH = WIDTH - 4 * PADDING;
 						Text itemDisplayName = new Text();
 						{
 							itemDisplayName.getStyleClass().add("itemDisplayName");
@@ -102,7 +159,7 @@ public class DesktopApplication extends Application{
 				}
 				itemDisplay.getChildren().addAll(background,itemDisplayInfoBorder);
 			}
-			HBox editItemMenu = new HBox(StageConstants.PADDING);
+			HBox editItemMenu = new HBox(PADDING);
 			{//set the content of the item list menu
 				editItemMenu.setMinSize(WIDTH, SECTION_HEIGHT);
 				editItemMenu.setMaxSize(WIDTH, SECTION_HEIGHT);
@@ -110,8 +167,8 @@ public class DesktopApplication extends Application{
 				editItemMenu.getStyleClass().add("itemListMenu");
 
 				final int NUMBER_OF_BUTTONS = 3;
-				final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * StageConstants.PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
-						BUTTON_HEIGHT = SECTION_HEIGHT - 2 * StageConstants.PADDING;
+				final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
+						BUTTON_HEIGHT = SECTION_HEIGHT - 2 * PADDING;
 				ToggleButton toggleFinished = new ToggleButton(this.activeItem.isValid() ? this.activeItem.get().getStatus().toString() : "Toggle Finished");
 				{
 					final Pair<Integer> BUTTON_SIZE = new Pair<>(BUTTON_WIDTH,BUTTON_HEIGHT);
@@ -162,17 +219,17 @@ public class DesktopApplication extends Application{
 	/**
 	 * Constructs the listPane which is the list of Items which the user can select to view more details
 	 */
-	private void constructListPane(){
+	private void updateListPane(){
 		this.listPane.getChildren().clear();
 		this.database.fillList();
 		final double WIDTH_PERCENT = .33;
-		final double WIDTH = StageConstants.DEFAULT_SIZE.width * WIDTH_PERCENT;
+		final double WIDTH = DEFAULT_SIZE.width * WIDTH_PERCENT;
 
 		this.listPane.setMaxWidth(WIDTH);
 		this.listPane.setPrefWidth(this.listPane.getMaxWidth());
 		this.listPane.getStyleClass().add("listPane");
 		{//the display for the list of items
-			HBox itemListMenu = new HBox(StageConstants.PADDING);
+			HBox itemListMenu = new HBox(PADDING);
 			{//set the content of the item list menu
 				itemListMenu.setMinSize(WIDTH, SECTION_HEIGHT);
 				itemListMenu.setMaxSize(WIDTH, SECTION_HEIGHT);
@@ -180,8 +237,8 @@ public class DesktopApplication extends Application{
 				itemListMenu.getStyleClass().add("itemListMenu");
 
 				final int NUMBER_OF_BUTTONS = 3;
-				final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * StageConstants.PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
-					BUTTON_HEIGHT = SECTION_HEIGHT - 2 * StageConstants.PADDING;
+				final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
+					BUTTON_HEIGHT = SECTION_HEIGHT - 2 * PADDING;
 				final Pair<Integer> BUTTON_SIZE = new Pair<>(BUTTON_WIDTH,BUTTON_HEIGHT);
 				Button addNew = new Button("Add New");
 				{
@@ -195,7 +252,7 @@ public class DesktopApplication extends Application{
 							(ActionEvent event) ->
 							{
 								this.rightDisplay = RightDisplay.ADD_NEW_ITEM;
-								constructRootPane();
+								updateRightPane();
 							}
 					);
 				}
@@ -248,9 +305,9 @@ public class DesktopApplication extends Application{
 					itemList.setPrefWidth(WIDTH);
 					itemList.setPrefViewportWidth(WIDTH);
 					{
-						VBox items = new VBox(StageConstants.PADDING);
+						VBox items = new VBox(PADDING);
 						util.Pair<Integer> BUTTON_SIZE = new util.Pair<>(
-								(int)(itemList.getPrefViewportWidth() - (4 * StageConstants.PADDING + util.Graphics.SCROLL_BAR_WIDTH)),
+								(int)(itemList.getPrefViewportWidth() - (4 * PADDING + util.Graphics.SCROLL_BAR_WIDTH)),
 								SECTION_HEIGHT
 						);
 						for(Item item : database.getItems()){
@@ -264,7 +321,8 @@ public class DesktopApplication extends Application{
 									{
 										this.rightDisplay = RightDisplay.ITEM_INFO;
 										this.activeItem.set(item);
-										constructRootPane();
+										updateInfoPane();
+										updateRightPane();
 									}
 							);
 							items.getChildren().add(button);
@@ -272,10 +330,10 @@ public class DesktopApplication extends Application{
 						itemList.setContent(items);
 					}
 					itemListBorder.getChildren().add(itemList);
-					AnchorPane.setBottomAnchor(itemList,(double)StageConstants.PADDING);
-					AnchorPane.setTopAnchor(itemList,(double)StageConstants.PADDING);
-					AnchorPane.setRightAnchor(itemList,(double)StageConstants.PADDING);
-					AnchorPane.setLeftAnchor(itemList,(double)StageConstants.PADDING);
+					AnchorPane.setBottomAnchor(itemList,(double) PADDING);
+					AnchorPane.setTopAnchor(itemList,(double) PADDING);
+					AnchorPane.setRightAnchor(itemList,(double) PADDING);
+					AnchorPane.setLeftAnchor(itemList,(double) PADDING);
 				}
 			}
 			this.listPane.getChildren().addAll(itemListMenu, itemListBorder);//order matters
@@ -285,11 +343,17 @@ public class DesktopApplication extends Application{
 	/**
 	 * Constructs a pane which provides for the ability of users to add new Items to the Database
 	 */
-	private void constructAddItemPane(){
+	private void updateAddItemPane(){
 		this.addItemPane.getChildren().clear();
-		this.infoPane.getChildren().clear();
+		{
+			Node toRemove = this.rootPane.lookup(INFO_PANE_ID);
+			if(toRemove != null) {
+				this.rootPane.getChildren().remove(toRemove);
+			}
+		}
+		//this.infoPane.getChildren().clear();
 		final double WIDTH_PERCENT = .66;
-		final double WIDTH = StageConstants.DEFAULT_SIZE.width * WIDTH_PERCENT;
+		final double WIDTH = DEFAULT_SIZE.width * WIDTH_PERCENT;
 
 		this.addItemPane.setMaxWidth(WIDTH);
 		this.addItemPane.setPrefWidth(this.addItemPane.getMaxWidth());
@@ -304,13 +368,13 @@ public class DesktopApplication extends Application{
 		{
 			addItemFieldsBorder.getStyleClass().add("addItemFieldsBorder");
 
-			final int BACKGROUND_WIDTH = (int)(WIDTH - 2 * StageConstants.PADDING), BACKGROUND_HEIGHT = 605;
+			final int BACKGROUND_WIDTH = (int)(WIDTH - 2 * PADDING), BACKGROUND_HEIGHT = 605;
 			Rectangle background = new Rectangle(BACKGROUND_WIDTH,BACKGROUND_HEIGHT);
 			background.getStyleClass().add("addItemFieldsBackground");
 
-			VBox addItemFields = new VBox(StageConstants.PADDING);
+			VBox addItemFields = new VBox(PADDING);
 			{
-				final int TEXT_INPUT_WIDTH = (int)(WIDTH - 4 * StageConstants.PADDING);
+				final int TEXT_INPUT_WIDTH = (int)(WIDTH - 4 * PADDING);
 				addItemFields.getStyleClass().add("addItemFields");
 				{
 					setName.getStyleClass().add("setName");
@@ -320,7 +384,7 @@ public class DesktopApplication extends Application{
 					setName.setMaxWidth(TEXT_INPUT_WIDTH);
 					setName.setPrefWidth(TEXT_INPUT_WIDTH);
 				}
-				HBox setPriority = new HBox(StageConstants.PADDING);
+				HBox setPriority = new HBox(PADDING);
 				{
 					setPriority.getStyleClass().add("setPriority");
 
@@ -350,11 +414,11 @@ public class DesktopApplication extends Application{
 			}
 			addItemFieldsBorder.getChildren().addAll(background,addItemFields);
 		}
-		HBox addItemMenu = new HBox(StageConstants.PADDING);
+		HBox addItemMenu = new HBox(PADDING);
 		{
 			final int NUMBER_OF_BUTTONS = 2;
-			final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * StageConstants.PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
-					BUTTON_HEIGHT = SECTION_HEIGHT - 2 * StageConstants.PADDING;
+			final int BUTTON_WIDTH = (int)((WIDTH - (NUMBER_OF_BUTTONS + 1) * PADDING) * (1.0 / NUMBER_OF_BUTTONS)),
+					BUTTON_HEIGHT = SECTION_HEIGHT - 2 * PADDING;
 			addItemMenu.getStyleClass().add("addItemMenu");
 			Button addItemButton = new Button("Add Item");
 			{
@@ -370,7 +434,9 @@ public class DesktopApplication extends Application{
 							Item.Priority itemPriority = prioritySelector.getValue();
 							if(!itemName.equals("")){
 								this.database.writeItem(new Item(itemName, itemDescription, itemPriority));
-								this.constructRootPane();
+								updateListPane();//TODO: replace with updateLeftPane when it exists
+								updateAddItemPane();
+								updateRightPane();
 							}
 						}
 				);
@@ -386,7 +452,8 @@ public class DesktopApplication extends Application{
 						(ActionEvent event) ->
 						{
 							this.rightDisplay = RightDisplay.ITEM_INFO;
-							constructRootPane();
+							updateAddItemPane();
+							updateRightPane();
 						}
 				);
 			}
@@ -398,26 +465,15 @@ public class DesktopApplication extends Application{
 	/**
 	 * Constructs the primary pane of the application which contains the listPane and either the infoPane or the addItemPane
 	 */
-	private void constructRootPane(){
+	private void updateRootPane(){
 		this.rootPane.getChildren().clear();
-		this.rootPane.setMaxSize(StageConstants.DEFAULT_SIZE.width,StageConstants.DEFAULT_SIZE.height);
+		this.rootPane.setMaxSize(DEFAULT_SIZE.width, DEFAULT_SIZE.height);
 		this.rootPane.getStyleClass().add("rootPane");
 
-		constructListPane();
+		removeNode(LIST_PANE_ID);//TODO: make own method like updateRightPane
 		this.rootPane.getChildren().add(this.listPane);
 
-		switch(this.rightDisplay) {
-			case ITEM_INFO:
-				constructInfoPane();
-				this.rootPane.getChildren().add(this.infoPane);
-				break;
-			case ADD_NEW_ITEM:
-				constructAddItemPane();
-				this.rootPane.getChildren().add(this.addItemPane);
-				break;
-			default:
-				Util.nyi(util.Util.getFileName(),util.Util.getLineNumber());
-		}
+		updateRightPane();
 	}
 
 	/**
@@ -429,11 +485,14 @@ public class DesktopApplication extends Application{
 		initialize();
 		this.mainStage = primaryStage;
 
-		constructRootPane();
+		updateListPane();
+		updateInfoPane();
+		updateAddItemPane();
+		updateRootPane();
 
 		this.mainStage.setTitle("Tempest");
 		this.mainStage.setResizable(RESIZABLE);
-		this.mainStage.setScene(new Scene(this.rootPane,StageConstants.DEFAULT_SIZE.width,StageConstants.DEFAULT_SIZE.height));
+		this.mainStage.setScene(new Scene(this.rootPane, DEFAULT_SIZE.width, DEFAULT_SIZE.height));
 		this.mainStage.getScene().getStylesheets().add(STYLESHEET_SOURCE);
 		this.mainStage.show();
 	}
@@ -457,7 +516,7 @@ public class DesktopApplication extends Application{
 		this.rootPane = new HBox();
 		this.rootPane.setId(ROOT_PANE_ID);
 
-		this.listPane = new VBox(StageConstants.PADDING);
+		this.listPane = new VBox(PADDING);
 		this.listPane.setId(LIST_PANE_ID);
 
 		this.infoPane = new VBox();
